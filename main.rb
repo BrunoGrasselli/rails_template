bf4a4d6acb9f85031fc3ee5f3907b9943077ddb4
Bruno Grasselli <bruno.grasselli@gmail.com>
2012-01-28 20:52:13 -0200

    using the environment method to add a line to config/environments/development.rb

--- main.rb
+++ main.rb
@@ -4,10 +4,13 @@ append_file '.gitignore', '*.sw[op]'
 
 run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/Gemfile -o Gemfile'
 run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/application.html.erb -o app/views/layouts/application.html.erb'
-run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/development.rb -o config/environments/development.rb'
 run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/factory_girl.rb -o features/support/factory_girl.rb'
 
 run 'bundle install'
+
+environment %{
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+}, :env => :development
 
 application %{
     config.generators do |g|
