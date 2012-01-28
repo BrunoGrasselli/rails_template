238d64ba691bdce39e01a1b152e1201064cb622c
Bruno Grasselli <bruno.grasselli@gmail.com>
2012-01-28 17:14:01 -0200

    adding some devise configurations

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 system 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/Gemfile -o Gemfile'
 system 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/.gitignore -o .gitignore'
 system 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/application.rb -o config/application.rb'
+system 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/application.html.erb -o app/views/layouts/application.html.erb'
+system 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/development.rb -o config/environments/development.rb'
 system 'rm -rf test/ public/index.html'
 system 'mkdir -p spec/support/factories'
 
