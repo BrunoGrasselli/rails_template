afdb2285ba5efdfade4e5bad8c269e360641f4d3
Bruno Grasselli <bruno.grasselli@gmail.com>
2012-01-28 19:27:45 -0200

    using run

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
-system 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/Gemfile -o Gemfile'
-system 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/.gitignore -o .gitignore'
-system 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/application.rb -o config/application.rb'
-system 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/application.html.erb -o app/views/layouts/application.html.erb'
-system 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/development.rb -o config/environments/development.rb'
-system 'rm -rf test/ public/index.html'
-system 'mkdir -p spec/support/factories'
+run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/Gemfile -o Gemfile'
+run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/.gitignore -o .gitignore'
+run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/application.rb -o config/application.rb'
+run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/application.html.erb -o app/views/layouts/application.html.erb'
+run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/development.rb -o config/environments/development.rb'
+run 'rm -rf test/ public/index.html'
+run 'mkdir -p spec/support/factories'
 
 run 'bundle install'
 
