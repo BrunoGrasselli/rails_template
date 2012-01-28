a717aeca65d3e1f284494a865a7dd2cdb11abb1a
Bruno Grasselli <bruno.grasselli@gmail.com>
2012-01-28 19:34:53 -0200

    using a better way to set the generators

--- main.rb
+++ main.rb
@@ -1,12 +1,19 @@
 run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/Gemfile -o Gemfile'
 run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/.gitignore -o .gitignore'
-run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/application.rb -o config/application.rb'
 run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/application.html.erb -o app/views/layouts/application.html.erb'
 run 'curl https://raw.github.com/BrunoGrasselli/rails_template/master/development.rb -o config/environments/development.rb'
+
 run 'rm -rf test/ public/index.html'
 run 'mkdir -p spec/support/factories'
 
 run 'bundle install'
+
+application %{
+    config.generators do |g|
+      g.test_framework  :rspec, :fixture => true, :views => false
+      g.fixture_replacement :factory_girl, :dir => "spec/support/factories"
+    end
+}
 
 generate 'rspec:install'
 generate 'cucumber:install'
